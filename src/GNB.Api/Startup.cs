797a2045012e29e7930f6b8c5769b5d05ae6f6d2
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2020-08-08 13:18:14 -0300

    #2: add swagger package with default configs

--- src/GNB.Api/Startup.cs
+++ src/GNB.Api/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace GNB.Api
 {
@@ -25,6 +19,8 @@ namespace GNB.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "GNB API", Version = "v1"}); });
+
             services.AddControllers();
         }
 
@@ -35,6 +31,13 @@ namespace GNB.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "GNB API");
+            });
 
             app.UseHttpsRedirection();
 
