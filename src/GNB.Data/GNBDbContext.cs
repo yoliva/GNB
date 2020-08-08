72e05d792a2ee545397d034ff03604011039a11b
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2020-08-08 17:26:38 -0300

    #5: create base DbContext

---
+++ src/GNB.Data/GNBDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GNB.Data
+{
+    public class GNBDbContext : IdentityDbContext
+    {
+        public GNBDbContext()
+        {
+        }
+
+        public GNBDbContext(DbContextOptions<GNBDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
