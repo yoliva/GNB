d9e2cc2afb0a2cb4edc62593227c6d94b37aede0
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2020-08-09 13:07:20 -0300

    #11: create QuietStoneApi service and tests

---
+++ src/GNB.Services/QuietStone/Dtos/QuietStoneRateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GNB.Services.QuietStone.Dtos
+{
+    public class QuietStoneRateDto
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public decimal Rate { get; set; }
+    }
+}
---
+++ src/GNB.Services/QuietStone/IQuietStoneApi.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using GNB.Services.QuietStone.Dtos;
+
+namespace GNB.Services.QuietStone
+{
+    public interface IQuietStoneApi
+    {
+        Task<IEnumerable<QuietStoneRateDto>> GetRates();
+    }
+}
