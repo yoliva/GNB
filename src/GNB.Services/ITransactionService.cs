b2dceea841bb6ddfc3d42fb6bc598b59896646b5
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2020-08-09 00:30:25 -0300

    #10: create TransactionService with methods to pull all the records or filtered bu sku

---
+++ src/GNB.Services/ITransactionService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using GNB.Services.Dtos;
+
+namespace GNB.Services
+{
+    public interface ITransactionService
+    {
+        IEnumerable<TransactionDto> GetTransactions();
+        IEnumerable<TransactionDto> GetTransactionsBySku(string sku);
+    }
+}
---
+++ src/GNB.Services/TransactionService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GNB.Core.UnitOfWork;
+using GNB.Services.Dtos;
+using Mapster;
+
+namespace GNB.Services
+{
+    public class TransactionService : ITransactionService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TransactionService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IEnumerable<TransactionDto> GetTransactions()
+        {
+            return _unitOfWork.TransactionRepository.GetAll()
+                .Select(x => x.Adapt<TransactionDto>())
+                .AsEnumerable();
+        }
+
+        public IEnumerable<TransactionDto> GetTransactionsBySku(string sku)
+        {
+            if (string.IsNullOrEmpty(sku))
+                throw new ArgumentNullException(nameof(sku));
+
+            return _unitOfWork.TransactionRepository.GetAll()
+                .Where(x => x.Sku == sku)
+                .Select(x => x.Adapt<TransactionDto>())
+                .AsEnumerable();
+        }
+    }
+}
