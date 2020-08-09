d029882636fb6e7d4457828cd17583547a887fa7
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2020-08-09 00:27:48 -0300

    #10: create TransactionDto and mapping to Transaction

--- src/GNB.Services/Mappings/MapsterConfig.cs
+++ src/GNB.Services/Mappings/MapsterConfig.cs
@@ -5,6 +5,7 @@
         public static void Configure()
         {
             RateMapping.Configure();
+            TransactionMapping.Configure();
         }
     }
 }
---
+++ src/GNB.Services/Mappings/TransactionMapping.cs
@@ -0,0 +1,15 @@
+﻿using GNB.Core;
+using GNB.Services.Dtos;
+using Mapster;
+
+namespace GNB.Services.Mappings
+{
+    public static class TransactionMapping
+    {
+        public static void Configure()
+        {
+            TypeAdapterConfig.GlobalSettings
+                .NewConfig<Transaction, TransactionDto>();
+        }
+    }
+}
