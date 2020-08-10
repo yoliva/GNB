29a4fa2941c718bb8db8cb36c7b40f634d77e9fb
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2020-08-10 14:06:13 -0300

    #14: create empty page to hold transaction table

--- src/GNB.Api/Controllers/TransactionsController.cs
+++ src/GNB.Api/Controllers/TransactionsController.cs
@@ -24,7 +24,7 @@ namespace GNB.Api.Controllers
 
         [HttpGet]
         [Route("sku/{sku}")]
-        public async Task<ActionResult> GetBySku(string sku, string currency = KnownCurrencies.Euro)
+        public async Task<ActionResult> GetBySku(string sku, string currency = KnownCurrencies.EURO)
         {
             return Ok(await _transactionService.GetTransactionsBySku(sku, currency));
         }
