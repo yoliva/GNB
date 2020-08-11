6c91bfc4de014d176a2382311ac82b96789ea70b
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2020-08-11 14:23:06 -0300

    #19: update transaction dashboard

---
+++ src/GNB.Api/Controllers/DashboardController.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using GNB.Api.Models;
+using GNB.Core;
+using GNB.Services;
+using GNB.Services.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GNB.Api.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly ITransactionService _transactionService;
+
+        public DashboardController(ITransactionService transactionService)
+        {
+            _transactionService = transactionService;
+        }
+
+        public async Task<IActionResult> Index(string sku, string currency = KnownCurrencies.EURO)
+        {
+            return View(new TransactionList
+            {
+                Sku = sku,
+                Transactions = string.IsNullOrEmpty(sku)
+                    ? Enumerable.Empty<TransactionDto>()
+                    : await _transactionService.GetTransactionsBySku(sku, currency)
+            });
+        }
+    }
+}
